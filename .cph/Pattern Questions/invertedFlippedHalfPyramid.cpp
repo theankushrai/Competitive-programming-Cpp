e1644fb1ab3d9010382c92ba50950e6a8369d196
theankushrai <iamankushrai@gmail.com>
2021-02-08 14:43:20 +0530

    first commit

---
+++ .cph/Pattern Questions/invertedFlippedHalfPyramid.cpp
@@ -0,0 +1,27 @@
+// * * * * * 
+//   * * * *
+//     * * *
+//       * *
+//         *
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i =5;i>0;i--){
+        for (int k = 0; k < 5-i; k++)
+        {
+            cout<<"  ";
+        }
+        
+        for (int j = 0; j < i; j++)
+        {
+            cout<<"* "; 
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
